391ce00bb80eca8bff8993bbcf7feb2190cea2a9
Christopher Wells <cwellsny@nycap.rr.com>
2016-12-18 21:21:52 -0500

    Implement search
    Implement the search method of the BinarySearchTree class.

--- src/main/scala/BinarySearchTree.scala
+++ src/main/scala/BinarySearchTree.scala
@@ -1,5 +1,7 @@
 package binarysearchtree
 
+import scala.annotation.tailrec
+
 case class BinarySearchTree[A <% Ordered[A]](item: A, left: Option[BinarySearchTree[A]], right: Option[BinarySearchTree[A]]) {
   def insert(v: A): BinarySearchTree[A] = {
     if (v <= this.item) {
@@ -15,6 +17,23 @@ case class BinarySearchTree[A <% Ordered[A]](item: A, left: Option[BinarySearchT
         case None =>
           val newRight = Some(BinarySearchTree[A](v, None, None))
           BinarySearchTree[A](this.item, this.left, newRight)
+      }
+    }
+  }
+
+  @tailrec
+  final def search(v: A): Boolean = {
+    if (v == this.item) {
+      true
+    } else if (v < this.item) {
+      this.left match {
+        case Some(lTree) => lTree.search(v)
+        case None => false
+      }
+    } else {
+      this.right match {
+        case Some(rTree) => rTree.search(v)
+        case None => false
       }
     }
   }
